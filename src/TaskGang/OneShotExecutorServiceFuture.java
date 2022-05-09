7dace69a789d0b404d5a9e7c62e731c5ce22dc2f
YeonginKim <fhtmah12@gmail.com>
2022-05-09 16:38:17 +0900

    OneShotThreadPerTask and OneShotThreadPerTask filled.

--- src/TaskGang/OneShotExecutorServiceFuture.java
+++ src/TaskGang/OneShotExecutorServiceFuture.java
@@ -1,4 +1,56 @@
 package TaskGang;
 
-public class OneShotExecutorServiceFuture {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class OneShotExecutorServiceFuture extends SearchTaskGangCommon{
+
+    protected List<Future<SearchResults>> mResultFutures;
+
+    protected OneShotExecutorServiceFuture(String[] wordsToFind, String[][] stringsToSearch) {
+
+        super(wordsToFind,
+                stringsToSearch);
+
+        setExecutor(Executors.newCachedThreadPool());
+    }
+
+    protected void initiateTaskGang(int inputSize) {
+
+        mResultFutures =
+                new ArrayList<>(inputSize
+                                * mWordsToFind.length);
+
+        for (final String inputData : getInput())
+            processInput(inputData);
+
+        processFutureResults(mResultFutures);
+    }
+
+    protected boolean processInput(String inputData) {
+        ExecutorService executorService =
+                (ExecutorService) getExecutor();
+
+        for (final String word : mWordsToFind) {
+
+            Future<SearchResults> resultFuture = executorService
+                    .submit(() -> searchForWord(word, inputData));
+            mResultFutures.add(resultFuture);
+        }
+        return true;
+    }
+
+    protected void processFutureResults
+            (List<Future<SearchResults>> mResultFutures) {
+        for (Future<SearchResults> resultFuture : mResultFutures) {
+            try {
+                resultFuture.get().print();
+            } catch (Exception e) {
+                System.out.println("get() exception");
+            }
+        }
+    }
 }
--- src/TaskGang/OneShotThreadPerTask.java
+++ src/TaskGang/OneShotThreadPerTask.java
@@ -1,4 +1,49 @@
 package TaskGang;
 
-public class OneShotThreadPerTask {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+
+public class OneShotThreadPerTask extends SearchTaskGangCommon{
+
+    private final List<Thread> mWorkerThreads;
+
+    public OneShotThreadPerTask(String[] wordsToFind, String[][] stringsToSearch) {
+
+        super(wordsToFind, stringsToSearch);
+
+        mWorkerThreads = new LinkedList<>();
+    }
+
+    protected void initiateTaskGang(int inputSize) {
+        if (getExecutor() == null)
+            setExecutor(r -> {
+                Thread thread = new Thread(r);
+                mWorkerThreads.add(thread);
+                thread.start();
+            });
+        for (int i = 0; i < inputSize; ++i)
+            getExecutor().execute(makeTask(i));
+    }
+
+    @Override
+    protected boolean processInput(String inputData) {
+        for (String word : mWordsToFind) {
+            SearchResults results = searchForWord(word, inputData);
+
+            synchronized (System.out) {
+                results.print();
+            }
+        }
+        return true;
+    }
+
+    protected void awaitTasksDone() {
+        for (Thread thread : mWorkerThreads)
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.out.println("awaitTasksDone interrupted");
+            }
+    }
 }
